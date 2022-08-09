81ea044df4fc250bc982fa988f32be40f846b303
Gokulcasper <ronitcasper215@gmail.com>
2022-08-09 23:45:33 +0530

    Feat : Add Todo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import { useState, useRef } from "react";
 import "./App.css";
 import TodoList from "./TodoList";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const inputRef = useRef();
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const val = inputRef.current.value;
+    const tmpTodo = [...todoList].push(val);
+    setTodoList(tmpTodo);
+    inputRef.current.value = "";
+  };
+
   return (
     <div className="App">
-      <h1>
-        <TodoList />
-      </h1>
-      <input />
-      <button>AddTodo</button>
+      <h1>TodoList</h1>
+      <TodoList todoList={todoList} />
+      <br />
+      <input ref={inputRef} />
+      <br /> <br />
+      <button onClick={handleAdd}>AddTodo</button> <tt />
       <button>Clear</button>
+      <p>Left Items</p>
     </div>
   );
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Todo from "./Todo";
 
-const TodoList = () => {
-  return <div>TodoList</div>;
+const TodoList = ({ todoList }) => {
+  return (
+    <div>
+      {todoList.map((todo) => (
+        <Todo key={todo} todo={todo} />
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
