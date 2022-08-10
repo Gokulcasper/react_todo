98dcd0d5cf9c662f73296070bc4cdf4ad583974d
Gokulcasper <ronitcasper215@gmail.com>
2022-08-10 22:35:56 +0530

    Feat : react_todo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import TodoList from "./TodoList";
 import { v4 as uuidv4 } from "uuid";
@@ -6,6 +6,16 @@ import { v4 as uuidv4 } from "uuid";
 function App() {
   const [todoList, setTodoList] = useState([]);
   const inputRef = useRef();
+
+  useEffect(() => {
+    const localVal = localStorage.getItem("TODOLIST");
+    setTodoList(JSON.parse(localVal));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("TODOLIST", JSON.stringify(todoList));
+  }, [todoList]);
+
   const handleAdd = () => {
     const val = inputRef.current.value;
     const tmpToDo = [...todoList];
@@ -21,6 +31,12 @@ function App() {
     setTodoList(tmpToDo);
   };
 
+  const clearComplete = () => {
+    const tmpToDo = [...todoList];
+    const unCompeteTodo = tmpToDo.filter((todo) => !todo.complete);
+    setTodoList(unCompeteTodo);
+  };
+
   return (
     <div className="App">
       <h1>TodoList</h1>
@@ -29,8 +45,8 @@ function App() {
       <input ref={inputRef} />
       <br /> <br />
       <button onClick={handleAdd}>AddTodo</button> <tt />
-      <button>Clear</button>
-      <p>Left Items</p>
+      <button onClick={clearComplete}>Clear</button>
+      <p>{todoList.filter((todo) => !todo.complete).length} Left Items</p>
     </div>
   );
 }
